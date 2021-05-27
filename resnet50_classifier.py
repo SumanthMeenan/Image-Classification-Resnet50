d1331ec69ba193c68cddf5bf76ef8e49c65fe6e8
SumanthMeenan <sumanthmeenan.kanneti123@gmail.com>
2021-05-27 13:22:17 +0530

    Add files via upload

--- resnet50_classifier.py
+++ resnet50_classifier.py
@@ -8,7 +8,7 @@ from vgg import VGG16
 from PIL import * 
 import numpy as np
 import pandas as pd
-# import tensorflow as tf
+import tensorflow as tf
 from keras import optimizers
 from keras import applications
 from keras import backend as K
@@ -21,6 +21,9 @@ from keras.callbacks import ModelCheckpoint, TensorBoard, ReduceLROnPlateau
 from keras.layers import Conv2D, MaxPooling2D, Dense, GlobalAveragePooling2D, Activation, Dropout, Flatten, Dense
 from keras.optimizers import SGD
 from keras.utils.vis_utils import plot_model
+from sklearn.metrics import confusion_matrix, plot_confusion_matrix
+import matplotlib 
+import matplotlib.pyplot as plt 
 
 train_datagen = ImageDataGenerator(
     rescale=1. / 255,
@@ -86,44 +89,57 @@ def train_vgg16(train_generator, validation_generator):
                         validation_steps=constants.NB_VALIDATION_SAMPLES // constants.BATCH_SIZE,callbacks=[constants.CHECKPOINTER, constants.TENSORBOARD])
     print(" Inside Train")
     # model.save_weights(constants.WEIGHTS_PATH2)
-    model.save(constants.MODELPATH)
+    model.save(constants.MODELPATH1)
 
-def build_resnet_model():
-    model = Sequential()
-    model.add(ResNet50(include_top = False, pooling = 'avg', weights = 'imagenet'))
-    model.add(Dense(constants.NUM_CLASSES, activation = 'softmax'))
-    model.layers[0].trainable = False
-    return model
+# def build_resnet_model():
+#     model = Sequential()
+#     model.add(ResNet50(include_top = False, pooling = 'avg', weights = 'imagenet'))
+#     model.add(Dense(constants.NUM_CLASSES, activation = 'softmax'))
+#     model.layers[0].trainable = False
+#     return model
 
-def test_resnet_model(test_image):
-    model = build_resnet_model()
-    model.load_weights(constants.CHECKPOINT_PATH)
-    im = cv2.imread(test_image)
-    im = im.resize([ 256, 256, 3])
-    print(model.predict(im))
-    # print(model.predict_proba(im))
+# def test_resnet_model(test_image):
+#     model = build_resnet_model()
+#     model.load_weights(constants.CHECKPOINT_PATH)
+#     im = cv2.imread(test_image)
+#     im = im.resize([ 256, 256, 3])
+#     print(model.predict(im))
+#     # print(model.predict_proba(im))
 
-def build_vgg_model():
-    model = Sequential()
-    model.add(ResNet50(include_top = False, pooling = 'avg', weights = 'imagenet'))
-    model.add(Dense(constants.NUM_CLASSES, activation = 'softmax'))
-    model.layers[0].trainable = False
-    return model
-
-def test_vgg_model(test_image):
-    model = build_vgg_model()
-    model.load_weights(constants.CHECKPOINT_PATH)
-    im = cv2.imread(test_image)
-    im = im.reshape([-1, 256, 256, 3])
-    print(model.predict_proba(im))
 
 def test_resnet():
     model = load_model(constants.MODELPATH)
     pred=model.predict_generator(test_generator, steps=len(test_generator), verbose=1)
+    y_true = test_generator.classes
+    y_pred = pred > 0.5
+    font = {
+            'family': 'Times New Roman',
+            'size': 12
+            }
+    matplotlib.rc('font', **font)
+    mat = confusion_matrix(y_true, y_pred)
+    plot_confusion_matrix(conf_mat=mat, figsize=(8, 8), show_normed=False)
+    plt.show()
     cl = np.round(pred)
     filenames=test_generator.filenames
     results=pd.DataFrame({"file":filenames,"pr":pred[:,0], "class":cl[:,0]})
-    results.to_csv("results.csv")
+    results.to_csv("resnetresults.csv")
+
+# def build_vgg_model():
+#     model = Sequential()
+#     model.add(VGG16(include_top = False, pooling = 'avg', weights = 'imagenet'))
+#     model.add(Dense(constants.NUM_CLASSES, activation = 'softmax'))
+#     model.layers[0].trainable = False
+#     return model
+
+def test_vgg():
+    model = load_model(constants.MODELPATH1)
+    pred=model.predict_generator(test_generator, steps=len(test_generator), verbose=1)
+    cl = np.round(pred)
+    filenames=test_generator.filenames
+    results=pd.DataFrame({"file":filenames,"pr":pred[:,0], "class":cl[:,0]})
+    results.to_csv("vggresults.csv")
+
 
 # def test_batch(img_folder):
 #     model = build_model()
@@ -148,7 +164,7 @@ if __name__ == "__main__":
         train_resnet50(train_generator, validation_generator)
     if sys.argv[1] == 'train_vgg16':
         train_vgg16(train_generator, validation_generator)
-    if sys.argv[1] == 'test':
+    if sys.argv[1] == 'test_resnet50':
         test_resnet() 
-    if sys.argv[1] == 'test_batch':
-        test_batch(sys.argv[2])
+    if sys.argv[1] == 'test_vgg16':
+        test_vgg() 
